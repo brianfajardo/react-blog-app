27e01955dd07a02f8e5dacd3314d00befc2730f1
Brian Fajardo <bmf5@hotmail.com>
2017-04-04 18:41:56 -0400

    Create posts_create component and scaffold route

---
+++ src/components/posts_create.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PostCreate extends Component {
+    render() {
+        return (
+            <div>Create a post</div>
+        )
+    }
+}
+
+export default PostCreate
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from './components/app'
-import PostsIndex from './components/posts_index'
+import PostIndex from './components/posts_index'
+import PostCreate from './components/posts_create'
 
 export default (
     <Route path='/' component={App}>
-        <IndexRoute component={PostsIndex} />
+        <IndexRoute component={PostIndex} />
+        <Route path='posts/new' component={PostCreate} />
     </Route>
 )
