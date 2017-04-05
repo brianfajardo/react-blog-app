69bedbbb5da4a756522939ef5fdf51d41868cb0c
Brian Fajardo <bmf5@hotmail.com>
2017-04-04 20:01:20 -0400

    Pass redux-form fields properties to PostCreate component inputs

--- src/components/posts_create.js
+++ src/components/posts_create.js
@@ -4,21 +4,31 @@ import { reduxForm } from 'redux-form'
 
 class PostCreate extends Component {
     render() {
+        const { fields: { title, categories, content }, handleSubmit } = this.props
+
         return (
-            <form>
+            // handleSubmit is an injected prop from redux-form
+            // this function tells redux-form the user is trying to submit this form and that it should check for validation
+            <form onSubmit={handleSubmit}>
                 <h3>Create a Post!</h3>
+
                 <div className='form-group'>
                     <label>Title</label>
-                    <input type='text' className='form-control' />
+                    {/*Destructuring the object 'title' from props and passing it into this input */}
+                    {/*Instead of formProps={title} --> this.form.formProps.PROPERTY/METHOD*/}
+                    <input type='text' className='form-control' {...title} />
                 </div>
+
                 <div className='form-group'>
                     <label>Categories</label>
-                    <input type='text' className='form-control' />
+                    <input type='text' className='form-control' {...categories} />
                 </div>
+
                 <div className='form-group'>
                     <label>Content</label>
-                    <textarea className='form-control' />
+                    <textarea className='form-control'{...content} />
                 </div>
+
                 <button type='submit' className='btn btn-primary'>
                     Submit
                 </button>
@@ -28,7 +38,8 @@ class PostCreate extends Component {
 }
 
 // Similar to connect function from react-redux library
-// Giving reduxForm instructions
+// Giving redux-form instructions
+// redux-form also injects props into our component -> console.log(this.props) in component
 export default reduxForm({
     form: 'PostCreate',
     fields: ['title', 'categories', 'content']
